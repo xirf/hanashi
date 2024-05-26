ff2e65fd394207e66ce8de9d4d7221de09ff9657
xirf <andikacahargen2@gmail.com>
2024-05-27 00:26:10 +0700

    feat: lanjut nanti jam 3 sore  mau turu

--- app/src/main/java/com/andka/hanashi/ui/homepage/MainActivity.kt
+++ app/src/main/java/com/andka/hanashi/ui/homepage/MainActivity.kt
@@ -3,24 +3,28 @@ package com.andka.hanashi.ui.homepage
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.andka.hanashi.R
 import com.andka.hanashi.databinding.ActivityMainBinding
 import com.andka.hanashi.ui.login.LoginActivity
+import com.andka.hanashi.utils.ResultState
+import kotlinx.coroutines.launch
+import org.xml.sax.Locator
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    private val fabClose by lazy { AnimationUtils.loadAnimation(this, R.anim.fab_close) }
-    private val fabOpen by lazy { AnimationUtils.loadAnimation(this, R.anim.fab_open) }
-    private val fabClock by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_fab_clock) }
-    private val fabAntiClock by lazy {
-        AnimationUtils.loadAnimation(
-            this,
-            R.anim.rotate_fab_anticlock
-        )
+    private val viewModel by viewModels<MainActivityViewModel> {
+        factoryProducer = { Locator.mainActivityViewModels }
     }
+    private val fabClose by lazy { getAnim(R.anim.fab_close) }
+    private val fabOpen by lazy { getAnim(R.anim.fab_open) }
+    private val fabClock by lazy { getAnim(R.anim.rotate_fab_clock) }
+    private val fabAntiClock by lazy { getAnim(R.anim.rotate_fab_anticlock) }
     private var isFabOpen = false;
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +37,23 @@ class MainActivity : AppCompatActivity() {
     private fun goToLogin() {
         startActivity(Intent(this, LoginActivity::class.java))
         finish()
+    }
+
+    private fun getAnim(id: Int): Animation {
+        return AnimationUtils.loadAnimation(this, id)
+    }
+
+    private fun checkLogin() {
+        lifecycleScope.launch {
+            viewModel.isLoggedIn.collect {
+                if (it.resultGetUser is ResultState.Success) {
+                    if (it.resultGetUser.data == false) {
+                        startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+                        finish()
+                    }
+                }
+            }
+        }
     }
 
     private fun toggleFab() {
--- app/src/main/java/com/andka/hanashi/utils/Locator.kt
+++ app/src/main/java/com/andka/hanashi/utils/Locator.kt
@@ -7,28 +7,37 @@ import com.andka.hanashi.data.repository.AuthRepository
 import com.andka.hanashi.data.repository.StoryRepository
 import com.andka.hanashi.data.source.local.UserPreferences
 import com.andka.hanashi.data.source.remote.ApiConfig
+import com.andka.hanashi.domain.usecase.GetStoriesUseCase
+import com.andka.hanashi.domain.usecase.GetUserUseCase
 import com.andka.hanashi.domain.usecase.LoginUseCase
+import com.andka.hanashi.domain.usecase.LogoutUseCase
 import com.andka.hanashi.domain.usecase.RegisterUseCase
+import com.andka.hanashi.ui.homepage.MainActivityViewModel
 import com.andka.hanashi.ui.login.LoginViewModel
 import com.andka.hanashi.ui.register.RegisterViewModel
 
 object Locator {
     private var application: Application? = null
 
-    private inline val requireApplication get() = application ?: error("Missing call: initWith(application)")
+    private inline val requireApplication
+        get() = application ?: error("Missing call: initWith(application)")
 
-    fun initWith(application: Application) { this.application = application }
+    fun initWith(application: Application) {
+        this.application = application
+    }
 
     private val Context.dataStore by preferencesDataStore(name = "user_preferences")
 
     val loginViewModelFactory get() = LoginViewModel.Factory(loginUseCase = loginUseCase)
     val registerViewModelFactory get() = RegisterViewModel.Factory(registerUseCase = registerUseCase)
+    val mainActivityViewModelFactory get() = MainActivityViewModel.Factory(getUserUseCase = getUserUseCase, logoutUseCase = logoutUseCase, getStoriesUseCase = getStoriesUseCase)
 
-    // UseCases Injection
     private val loginUseCase get() = LoginUseCase(userPreferencesRepository, authRepository)
     private val registerUseCase get() = RegisterUseCase(authRepository)
+    private val getStoriesUseCase get() = GetStoriesUseCase(storyRepository)
+    private val logoutUseCase get() = LogoutUseCase(userPreferencesRepository)
+    private val getUserUseCase get() = GetUserUseCase(userPreferencesRepository)
 
-    // Repository Injection
     private val userPreferencesRepository by lazy { UserPreferences(requireApplication.dataStore) }
     private val authRepository by lazy { AuthRepository(ApiConfig(requireApplication.dataStore).getApiService()) }
     private val storyRepository by lazy { StoryRepository(ApiConfig(requireApplication.dataStore).getApiService()) }
